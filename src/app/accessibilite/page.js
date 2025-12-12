b4d74d30b5a9f2613fceae5f061d8b6e9f302166
Slapsho <sanniervictor@gmail.com>
2025-12-12 16:07:09 +0100

    ajout des corrections

--- src/app/accessibilite/page.js
+++ src/app/accessibilite/page.js
@@ -1,10 +1,6 @@
 import { Container } from 'react-bootstrap';
 import styles from '../mentions-legales/legal.module.scss';
 
-export const metadata = {
-  title: 'Accessibilité - Trouve ton artisan',
-  description: 'Déclaration d\'accessibilité du site',
-};
 
 export default function AccessibilitePage() {
   return (
